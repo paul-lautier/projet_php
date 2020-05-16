809c0acfe6282a9c20d5f6ff7b2ff405c613523c
paul <paul@pop-os.localdomain>
2020-05-16 17:24:51 +0200

    correction de redirection + ajout profile

---
+++ donneur/profile_donneur.php
@@ -0,0 +1,20 @@
+<?php
+require '../function/connexion_test.php';
+
+if (!is_connected()){
+    header('Location: connexion.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="../CRUD/change_pass.php">changer son mot de passe</a>
+    
+</body>
+</html>
