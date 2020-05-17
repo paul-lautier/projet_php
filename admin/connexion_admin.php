757e0a19d4cba648842c8378524348ca080ea2b6
paul <paul@pop-os.localdomain>
2020-05-17 14:15:02 +0200

    add home admin + connexion pour admin

--- admin/connexion_admin.php
+++ admin/connexion_admin.php
@@ -1,21 +1,70 @@
+<?php require '../function/connexion_test.php'; ?>
+
 <?php
+$database_host = 'localhost';
+$database_port = '3306';
+$database_dbname = 'login';
+$database_user = 'root';
+$database_password = 'Paul@123';
+$database_charset = 'UTF8';
+$database_options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+];
+
+$pdo = new PDO(
+    'mysql:host=' . $database_host .
+    ';port=' . $database_port .
+    ';dbname=' . $database_dbname .
+    ';charset=' . $database_charset,
+    $database_user,
+    $database_password,
+    $database_options
+);
+
+
+
+if (isset ($_POST["connexion"])){
+    $username = $_POST["username"];
+    $password = ($_POST["password"]);
+}
+
+$query_verif = $pdo->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+$query_verif->execute([$username,$password]);
+
+if (is_connected()){
+    header('Location: ./home_admin.php');
+}
+
+if (!empty($_POST['username']) && !empty($_POST['password'])){
+    if ($query_verif->rowCount()>0){
+        session_start();
+        $_SESSION['connected'] = $username;
+        header('Location: ./home_admin.php');
+        exit;
+
+    }
+    elseif ($query_verif->rowCount() == 0){
+        echo "<script type='text/javascript'>alert(l identifiant et le mot de passe ne correspondent pas);</script>";
+    }
+}
+
 
 
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>connexion</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <form action="">
-        <input type="username" placeholder="username">
-        <input type="password"placeholder="password">
-        <button action="submit">se connecter</button>
-
+<form action="" method="post">
+        <input type="username" placeholder="username" name ="username">
+        <input type="password"placeholder="password" name="password">
+        <button action="submit" name="connexion">se connecter</button>
     </form>
-    
 </body>
 </html>
