ed7929fffe9b0d7f240e24806dc800baf12de305
paul <paul@pop-os.localdomain>
2020-05-17 02:21:57 +0200

    résolution du bug de changement de mdp

--- demandeur/change_pass.php
+++ demandeur/change_pass.php
@@ -7,9 +7,7 @@ if (!is_connected()){
     header('Location: ../connexion.php');
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 $database_host = 'localhost';
 $database_port = '3306';
 $database_dbname = 'login';
@@ -32,60 +30,57 @@ $pdo = new PDO(
 );
 
 
-$username = $_SESSION['connected']
+$username = $_SESSION['connected'];
 
 ?>
-    <p>changement de mdp pour l'utilisateur <?php echo "$username" ?></p>
-<form method="post">
-        <label>mot de passe actuel</label>
-        <input type="password" name="old_pass"><br>
-        <label>nouveau mot de passe</label>
-        <input type="password" name="new_pass"><br>
-        <label>comfirmation nouveau mot de passe</label>
-        <input type="password" name="new_pass_verif"><br>
 
-        <button type="submit">changer le mot de pass</button>
+    <form method="post">
+        
+        <input type="password" name="old_pass" placeholder="mot de passe actuel">
+        
+        <input type="password" name="new_pass" placeholder="nouveau mot de passe">
+
+        <input type="password" name="new_pass_verif" placeholder="comfirmation nouveau mot de passe">
+
+        <button type="submit">changer le mot de passe</button>
     </form>
     
 
 <?php
 
-if (isset($_POST['old_pass']) && isset($_POST['new_pass']) && isset($_POST['new_pass_vérif'])){
+if (isset($_POST["old_pass"]) && isset($_POST["new_pass"]) && isset($_POST["new_pass_verif"])){
+  
     $old_pass = md5($_POST['old_pass']);
     $new_pass = md5($_POST['new_pass']);
     $new_pass_verif = md5($_POST['new_pass_verif']);
-
-    // $querry_verif_compte = $pdo->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
-    // $querry_verif_compte->bindParam(":username",$username);
-    // $querry_verif_compte->bindParam(":password",$old_pass);
-    // $querry_verif_compte->execute();
+    
    
 
-    // $query_verif_old_pass = $pdo->prepare("SELECT users from users where password = ?");
-    // $querry_verif_compte->bindParam(':password',$new_pass);
-    // $query_verif_old_pass->execute();
+    $query_verif_old_pass = $pdo->prepare("SELECT password from users where password = :password");
+    $query_verif_old_pass->bindParam(':password',$old_pass);
+    $query_verif_old_pass->execute();
 
-    // if (!isset($_POST["old_pass"]) && !isset($_POST["new_pass"]) && !isset($_POST["new_pass_vérif"])){
-    //     echo "<script type='text/javascript'>alert('veuillez remplir les champs');</script>";
-    // }
-    // elseif($querry_verif_compte->rowCount() == 0 ){
-    //     echo "<script type='text/javascript'>alert('erreur d'identifiant ou de come de passe');</script>";
-    // }
-    // elseif ($query_verif_old_pass->rowCount() == 0){
-    //     echo "<script type='text/javascript'>alert('votre ancien mot de passe ne correspond pas');</script>";
-    //     }
+    if (!isset($_POST["old_pass"]) && !isset($_POST["new_pass"]) && !isset($_POST["new_pass_vérif"])){
+        echo "<script type='text/javascript'>alert('veuillez remplir les champs');</script>";
+    }
 
-    // elseif ($new_pass !== $new_pass_verif) {
-    //     echo "<script type='text/javascript'>alert('les deux mot de passes ne correspondent pas');</script>";
-    // }
+    if ($query_verif_old_pass->rowCount() == 0){
+        echo "<script type='text/javascript'>alert('votre ancien mot de passe ne correspond pas');</script>";
+        }
 
-    // else {
+    if ($new_pass !== $new_pass_verif) {
+        echo "<script type='text/javascript'>alert('les deux mot de passes ne correspondent pas');</script>";
+    }
 
+    else{
+        
 
-        $querr_change_info = $pdo->prepare("UPDATE users SET password = :new_pass where password = :old_pass");
-        $querr_change_info->execute([':new_pass',$new_pass,':old_pass',$old_pass]);
+        $querry_change_info = $pdo->prepare("UPDATE users SET password = :password WHERE password = :old_pass");
+        $querry_change_info->bindparam(":password", $new_pass);
+        $querry_change_info->bindparam(":old_pass", $old_pass);
+        $querry_change_info->execute();
         echo "<script type='text/javascript'>alert('changement éffectué');</script>";
-        // }
+        }
 
     }
 
